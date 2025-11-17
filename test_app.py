e697f6c210befd78aa100cfa34a00bafdcb13917
GuSantoz <gustavobomfimdossantos@gmail.com>
2025-11-17 20:21:44 -0300

    Atualizado testes

--- test_app.py
+++ test_app.py
@@ -2,29 +2,28 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para adicionar o atributo '__version__' em werkzeug 
 if not hasattr(werkzeug, '__version__'): 
     werkzeug.__version__ = "mock-version"
 
 class APITestCase(unittest.TestCase): 
-    @classmethod 
-    def setUpClass(cls): 
-        # Criação do cliente de teste 
-        cls.client = app.test_client()
+
+    def setUp(self): 
+        self.client = app.test_client()
     
-    def test_home(self):
+    def test_home_endpoint_returns_ok_and_message(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
+        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.json.get("message"), "API is running")
 
-    def test_login(self):
+    def test_login_endpoint_provides_token(self):
         response = self.client.post('/login')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
+        self.assertEqual(response.status_code, 200) 
+        self.assertTrue('access_token' in response.json)
 
-    def test_protected_no_token(self):
+    def test_protected_endpoint_is_unauthorized_without_token(self):
+        """ Testa se a rota protegida (/protected) barra o acesso sem token. """
         response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 401) 
 
 if __name__ == '__main__':
     unittest.main()
